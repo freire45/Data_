3c582c5e28bcd8b208256b2adbf2c762a5419a20
Erick Freire <erilefr@gmail.com>
2022-11-04 20:00:25 -0300

    V1
    V1

---
+++ Data/src/br/com/erickfreire/data/Data.java
@@ -0,0 +1,47 @@
+package br.com.erickfreire.data;
+
+public class Data {
+	
+	private int dia;
+	private int mes;
+	private int ano;
+	
+	public Data(int dia, int mes, int ano) {
+		super();
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public void setDia(int dia) {
+		this.dia = dia;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public void setMes(int mes) {
+		this.mes = mes;
+	}
+
+	public int getAno() {
+		return ano;
+	}
+
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	
+	public void mostraData() {
+		System.out.printf("%d/%d/%d%n", dia, mes, ano);
+	}
+	
+	
+	
+
+}
---
+++ Data/src/br/com/erickfreire/data/DataTeste.java
@@ -0,0 +1,11 @@
+package br.com.erickfreire.data;
+
+public class DataTeste {
+
+	public static void main(String[] args) {
+		Data d1 = new Data(04,11,2022);
+		
+		d1.mostraData();
+	}
+
+}
